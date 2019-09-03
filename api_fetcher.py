dcf83b11e8990ccf9efb30fc0e4ad76365a3945b
Martin Forstner <martin@redislabs.com>
2019-09-03 15:50:36 +0200

    refactor into classes

--- api_fetcher.py
+++ api_fetcher.py
@@ -1,95 +1,94 @@
 import base64
 import json
 import logging
-import os
 import re
 import ssl
 
 from urllib import request
 
-FQDN = os.getenv('CLUSTER_FQDN')
-USERNAME = os.getenv('CLUSTER_USERNAME')
-PASSWORD = os.getenv('CLUSTER_PASSWORD')
-
 SSL_CONTEXT = ssl.create_default_context()
 SSL_CONTEXT.check_hostname = False
 SSL_CONTEXT.verify_mode = ssl.CERT_NONE
 
-CACHE = {}
 
-
-def _do_http(_url, _user, _pass, _data=None, _method='GET'):
+class ApiFetcher(object):
     """
-    Perfrom an HTTP request and get the JSON response.
-
-    :param _url: The url of the request.
-    :param _user: The username.
-    :param _pass: The password.
-    :param _data: The request body, defaults to None
-    :param _method: The request method, defaults to GET.
-    :raise Exception: In case of non-200 HTTP status code.
-    :return: The response of the request.
+    API Fetcher class.
     """
-    req = request.Request(_url, method=_method)
 
-    credentials = ('%s:%s' % (_user, _pass))
-    encoded_credentials = base64.b64encode(credentials.encode('ascii'))
-    req.add_header('Authorization', 'Basic %s' % encoded_credentials.decode("ascii"))
+    def __init__(self, _fqdn, _username, _password):
+        """
+        :param _fqdn: The FQDN of the cluster.
+        :param _username: The username of the cluster.
+        :param _password: The password of the cluster.
+        """
+        self.fqdn = _fqdn
+        self.username = _username
+        self.password = _password
+        self.cache = {}
 
-    if _data:
-        req.add_header('Content-Type', 'application/json; charset=utf-8')
-        req.data(json.dumps(_data).encode('utf-8'))
+    def get_cluster_value(self, _key):
+        return self._fetch('cluster')[_key]
 
-    logging.debug('calling urlopen to {} ...'.format(_url))
-    rsp = request.urlopen(req, context=SSL_CONTEXT)
-    if rsp.code == 200:
-        return json.loads(rsp.read())
-    else:
-        raise Exception(str(rsp))
+    def get_shards_limit(self):
+        rsp = self._fetch('license')
+        match = re.search(r'Shards limit : (\d+)\n', rsp['license'], re.MULTILINE | re.DOTALL)
+        return match.group(1)
 
+    def get_shards_count(self):
+        rsp = self._fetch('shards')
+        return len(rsp)
 
-def _fetch(_topic):
-    if _topic in CACHE:
-        return CACHE[_topic]
-    else:
-        rsp = _do_http('https://{}:9443/v1/{}'.format(FQDN, _topic), USERNAME, PASSWORD)
-        CACHE[_topic] = rsp
-        return rsp
+    def get_number_of_nodes(self):
+        rsp = self._fetch('nodes')
+        return len(rsp)
 
+    def get_node_values(self, _key):
+        return [node[_key] for node in self._fetch('nodes')]
 
-def get_cluster_value(_key):
-    return _fetch('cluster')[_key]
+    def get_summed_node_values(self, _key):
+        return sum([node[_key] for node in self._fetch('nodes')])
 
+    def get_bdb_value(self, _bdb_id, _key):
+        return self._fetch(f'bdbs/{_bdb_id}')[_key]
 
-def get_shards_limit():
-    rsp = _fetch('license')
-    match = re.search(r'Shards limit : (\d+)\n', rsp['license'], re.MULTILINE | re.DOTALL)
-    return match.group(1)
+    def get_bdb_alerts(self, _bdb_id=None):
+        return self._fetch(f'bdbs/alerts/{_bdb_id}') if _bdb_id else self._fetch('bdbs/alerts')
 
+    def _fetch(self, _topic):
+        if _topic in self.cache:
+            return self.cache[_topic]
+        else:
+            rsp = self._do_http('https://{}:9443/v1/{}'.format(self.fqdn, _topic), self.username, self.password)
+            self.cache[_topic] = rsp
+            return rsp
 
-def get_shards_count():
-    rsp = _fetch('shards')
-    return len(rsp)
+    @staticmethod
+    def _do_http(_url, _user, _pass, _data=None, _method='GET'):
+        """
+        Perfrom an HTTP request and get the JSON response.
 
+        :param _url: The url of the request.
+        :param _user: The username.
+        :param _pass: The password.
+        :param _data: The request body, defaults to None
+        :param _method: The request method, defaults to GET.
+        :raise Exception: In case of non-200 HTTP status code.
+        :return: The response of the request.
+        """
+        req = request.Request(_url, method=_method)
 
-def get_number_of_nodes():
-    rsp = _fetch('nodes')
-    return len(rsp)
+        credentials = ('%s:%s' % (_user, _pass))
+        encoded_credentials = base64.b64encode(credentials.encode('ascii'))
+        req.add_header('Authorization', 'Basic %s' % encoded_credentials.decode("ascii"))
 
+        if _data:
+            req.add_header('Content-Type', 'application/json; charset=utf-8')
+            req.data(json.dumps(_data).encode('utf-8'))
 
-def get_nodes_values(_key):
-    return [node[_key] for node in _fetch('nodes')]
-
-
-def get_summed_nodes_values(_key):
-    return sum([node[_key] for node in _fetch('nodes')])
-
-
-def get_bdb_value(_bdb_id, _key):
-    return _fetch(f'bdbs/{_bdb_id}')[_key]
-
-
-def get_bdb_alerts(_bdb_id=None):
-    return _fetch(f'bdbs/alerts/{_bdb_id}') if _bdb_id else _fetch('bdbs/alerts')
-
-
+        logging.debug('calling urlopen to {} ...'.format(_url))
+        rsp = request.urlopen(req, context=SSL_CONTEXT)
+        if rsp.code == 200:
+            return json.loads(rsp.read())
+        else:
+            raise Exception(str(rsp))
--- ssh_rex.py
+++ ssh_rex.py
@@ -1,134 +1,131 @@
 import logging
-import os
 
 from subprocess import Popen, PIPE
 
-SSH_USERNAME = os.getenv('SSH_USERNAME')
-SSH_HOSTNAMES = os.getenv('SSH_HOSTNAMES')
-SSH_KEYFILE = os.getenv('SSH_KEYFILE')
 
-
-def _exec_on_all_nodes(_cmd):
-    hostnames = eval(SSH_HOSTNAMES)
-    return [_exec_on_ip(_cmd, hostname) for hostname in hostnames]
-
-
-def _exec_on_ip(_cmd, _ip):
+class SshRemoteExecutor(object):
     """
-    Execute a ssh remote command.
-
-    :param _cmd: The command to execute.
-    :param _ip: The IP address of the remote machine.
-    :return: The reponse.
-    :raise Exception: If an error occurred.
+    SSH Remote Executor class.
     """
-    return _exec_ssh(SSH_USERNAME, _ip, SSH_KEYFILE, _cmd)
 
+    def __init__(self, _usernae, _hostnames, _keyfile):
+        """
+        :param _usernae: The ssh username to log in.
+        :param _hostnames: A list with hostnames to log in.
+        :param _keyfile: The path to the ssh identity file.
+        """
+        self.username = _usernae
+        self.hostnames = _hostnames
+        self.keyfile = _keyfile
 
-def _exec_on_node(_cmd, _node_nr):
-    """
-    Execute a ssh remote command an a configured node.
+    def get_log_file_paths(self):
+        return self._exec_on_all_nodes('df -h /var/opt/redislabs/log')
 
-    :param _cmd: The command to execute.
-    :param _node_nr: The index in the array of the configured IP addresses.
-    :return: The response.
-    :raise Excpetion: If an error occurred.
-    """
-    hostname = eval(SSH_HOSTNAMES)[_node_nr]
-    return _exec_ssh(SSH_USERNAME, hostname, SSH_KEYFILE, _cmd)
+    def get_tmp_file_path(self):
+        return self._exec_on_all_nodes('df -h /tmp')
 
+    def get_quorums(self, _nr_of_nodes):
+        cmd = 'sudo /opt/redislabs/bin/rladmin info node {} | grep quorum'
+        return [self._exec_on_node(cmd.format(i), 0) for i in range(1, _nr_of_nodes + 1)]
 
-def _exec_ssh(_user, _host, _keyfile, _cmd):
-    """
-    Execute a remote command.
+    def get_master_node(self):
+        cmd = 'sudo /opt/redislabs/bin/rladmin status | grep master'
+        return self._exec_on_node(cmd, 0)
 
-    :param _user: The remote username.
-    :param _host: The remote machine.
-    :param _keyfile: The private keyfile.
-    :param _cmd: The command to execute.
-    :return: The response.
-    :raise Exception: If an error occurred.
-    """
-    return _exec_cmd(' '.join(['ssh', '-i {}'.format(_keyfile), '{}@{}'.format(_user, _host), '-C', _cmd]))
+    def get_swappiness(self, _node_nr=0):
+        cmd = 'grep swap /etc/sysctl.conf || echo inactive'
+        return self._exec_on_node(cmd, _node_nr)
 
+    def get_transparent_hugepages(self, _node_nr=0):
+        cmd = 'cat /sys/kernel/mm/transparent_hugepage/enabled'
+        return self._exec_on_node(cmd, _node_nr)
 
-def _exec_cmd(_args):
-    """
-    Execute a shell command.
+    def run_rladmin_status(self, _node_nr=0):
+        cmd = 'sudo /opt/redislabs/bin/rladmin status extra all'
+        return self._exec_on_node(cmd, _node_nr)
 
-    :param _args: The command string.
-    :return: The response.
-    :raise Exception: If an error occurred.
-    """
-    logging.debug(_args)
-    proc = Popen(_args, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-    proc.wait()
-    if proc.returncode == 0:
-        rsp = proc.stdout.read().decode('utf-8')
-        return rsp
-    else:
-        rsp = proc.stderr.read().decode('utf-8')
-        raise Exception(rsp)
+    def run_rlcheck(self, _node_nr=0):
+        cmd = 'sudo /opt/redislabs/bin/rlcheck'
+        return self._exec_on_node(cmd, _node_nr)
 
+    def run_cnm_ctl_status(self, _node_nr=0):
+        cmd = 'sudo /opt/redislabs/bin/cnm_ctl status'
+        return self._exec_on_node(cmd, _node_nr)
 
-def get_log_file_paths():
-    return _exec_on_all_nodes('df -h /var/opt/redislabs/log')
+    def run_supervisorctl_status(self, _node_nr=0):
+        cmd = 'sudo /opt/redislabs/bin/supervisorctl status'
+        return self._exec_on_node(cmd, _node_nr)
 
+    def find_errors_in_syslog(self, _node_nr=0):
+        cmd = 'sudo grep error /var/log/syslog || echo no error'
+        return self._exec_on_node(cmd, _node_nr)
 
-def get_tmp_file_path():
-    return _exec_on_all_nodes('df -h /tmp')
+    def find_errors_in_install_log(self, _node_nr=0):
+        cmd = 'zgrep error /var/opt/redislabs/log/install.log || echo no error'
+        return self._exec_on_node(cmd, _node_nr)
 
+    def find_errors_in_logs(self, _node_nr=0):
+        cmd = 'grep error /var/opt/redislabs/log/*.log || echo no error'
+        return self._exec_on_node(cmd, _node_nr)
 
-def get_quorums(_nr_of_nodes):
-    cmd = 'sudo /opt/redislabs/bin/rladmin info node {} | grep quorum'
-    return [_exec_on_node(cmd.format(i), 0) for i in range(1, _nr_of_nodes + 1)]
+    def _exec_on_all_nodes(self, _cmd):
+        """
+        Execut3e a shh remote command on all configured nodes.
+        :param _cmd:
+        :return:
+        """
+        return [self._exec_on_ip(_cmd, hostname) for hostname in self.hostnames]
 
+    def _exec_on_ip(self, _cmd, _ip):
+        """
+        Execute a ssh remote command.
 
-def get_master_node():
-    cmd = 'sudo /opt/redislabs/bin/rladmin status | grep master'
-    return _exec_on_node(cmd, 0)
+        :param _cmd: The command to execute.
+        :param _ip: The IP address of the remote machine.
+        :return: The reponse.
+        :raise Exception: If an error occurred.
+        """
+        return self._exec_ssh(self.username, _ip, self.keyfile, _cmd)
 
+    def _exec_on_node(self, _cmd, _node_nr):
+        """
+        Execute a ssh remote command an a configured node.
 
-def get_swappiness(_node_nr=0):
-    cmd = 'grep swap /etc/sysctl.conf || echo inactive'
-    return _exec_on_node(cmd, _node_nr)
+        :param _cmd: The command to execute.
+        :param _node_nr: The index in the array of the configured IP addresses.
+        :return: The response.
+        :raise Excpetion: If an error occurred.
+        """
+        return self._exec_ssh(self.username, self.hostnames[_node_nr], self.keyfile, _cmd)
 
+    def _exec_ssh(self, _user, _host, _keyfile, _cmd):
+        """
+        Execute a remote command.
 
-def get_transparent_hugepages(_node_nr=0):
-    cmd = 'cat /sys/kernel/mm/transparent_hugepage/enabled'
-    return _exec_on_node(cmd, _node_nr)
+        :param _user: The remote username.
+        :param _host: The remote machine.
+        :param _keyfile: The private keyfile.
+        :param _cmd: The command to execute.
+        :return: The response.
+        :raise Exception: If an error occurred.
+        """
+        return self._exec_cmd(' '.join(['ssh', '-i {}'.format(_keyfile), '{}@{}'.format(_user, _host), '-C', _cmd]))
 
+    @staticmethod
+    def _exec_cmd(_args):
+        """
+        Execute a shell command.
 
-def run_rladmin_status(_node_nr=0):
-    cmd = 'sudo /opt/redislabs/bin/rladmin status extra all'
-    return _exec_on_node(cmd, _node_nr)
-
-
-def run_rlcheck(_node_nr=0):
-    cmd = 'sudo /opt/redislabs/bin/rlcheck'
-    return _exec_on_node(cmd, _node_nr)
-
-
-def run_cnm_ctl_status(_node_nr=0):
-    cmd = 'sudo /opt/redislabs/bin/cnm_ctl status'
-    return _exec_on_node(cmd, _node_nr)
-
-
-def run_supervisorctl_status(_node_nr=0):
-    cmd = 'sudo /opt/redislabs/bin/supervisorctl status'
-    return _exec_on_node(cmd, _node_nr)
-
-
-def find_errors_in_syslog(_node_nr=0):
-    cmd = 'sudo grep error /var/log/syslog || echo no error'
-    return _exec_on_node(cmd, _node_nr)
-
-
-def find_errors_in_install_log(_node_nr=0):
-    cmd = 'zgrep error /var/opt/redislabs/log/install.log || echo no error'
-    return _exec_on_node(cmd, _node_nr)
-
-
-def find_errors_in_logs(_node_nr=0):
-    cmd = 'grep error /var/opt/redislabs/log/*.log || echo no error'
-    return _exec_on_node(cmd, _node_nr)
+        :param _args: The command string.
+        :return: The response.
+        :raise Exception: If an error occurred.
+        """
+        logging.debug(_args)
+        proc = Popen(_args, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+        proc.wait()
+        if proc.returncode == 0:
+            rsp = proc.stdout.read().decode('utf-8')
+            return rsp
+        else:
+            rsp = proc.stderr.read().decode('utf-8')
+            raise Exception(rsp)
