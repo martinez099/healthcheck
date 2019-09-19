6fb19db4999f0531b39192c2be20966bc46ccd69
Martin Forstner <martin@redislabs.com>
2019-09-19 08:39:32 +0200

    add warning and error checks to log suite

--- healthcheck/check_suites/suite_logs.py
+++ healthcheck/check_suites/suite_logs.py
@@ -4,9 +4,24 @@ from healthcheck.check_suites.base_suite import BaseCheckSuite
 class LogChecks(BaseCheckSuite):
     """Check logs via API"""
 
-    def check_logs(self, *_args, **_kwargs):
-        """get logs"""
+    def check_warnings(self, *_args, **_kwargs):
+        """get warnings"""
         logs = self.api.get('logs')
 
-        kwargs = {'alerts': logs}
+        kwargs = {'warnings': []}
+        for log in logs:
+            if log['severity'] == 'WARNING':
+                kwargs['warnings'].append(log)
+
+        return None, kwargs
+
+    def check_errors(self, *_args, **_kwargs):
+        """get errors"""
+        logs = self.api.get('logs')
+
+        kwargs = {'errors': []}
+        for log in logs:
+            if log['severity'] == 'ERROR':
+                kwargs['errors'].append(log)
+
         return None, kwargs
