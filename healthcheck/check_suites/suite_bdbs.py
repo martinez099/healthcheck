b09006f932be27b4a9e5eca74fa4bf49b44638a1
Martin Forstner <martin@redislabs.com>
2019-10-01 23:55:38 +0200

    add default bdb parameters

--- healthcheck/check_suites/suite_bdbs.py
+++ healthcheck/check_suites/suite_bdbs.py
@@ -17,28 +17,32 @@ class BdbChecks(BaseCheckSuite):
 
         return all(results), kwargs
 
-    def check_bdb_alert_settings(self, *_args, **_kwargs):
+    def _check_bdb_alert_settings(self, *_args, **_kwargs):
         """get database alert settings"""
         alerts = self.api.get('bdbs/alerts')
 
         return None, {'alerts': alerts}
 
     def check_bdbs(self, *_args, **_kwargs):
-        """check bdbs"""
+        """check databases"""
         bdbs = self.api.get('bdbs')
         results = []
         for bdb in bdbs:
+            values = _kwargs['__default__']
             if bdb['name'] in _kwargs:
-                result = self._check_bdb(bdb['uid'], _kwargs[bdb['name']])
-                results.append(result)
+                values.update(_kwargs[bdb['name']])
+            result = self._check_bdb(bdb['uid'], values)
+            results.append(result)
+
         return results
 
-    def _check_bdb(self, _uid, _dict):
+    def _check_bdb(self, _uid, _values):
         f"""check bdb:{_uid}"""
         bdb = self.api.get(f'bdbs/{_uid}')
-        result, kwargs = {}, {'bdb_id': _uid}
-        for k, v in _dict.items():
+        result, kwargs = {}, {'name': bdb['name'], 'not satisfied': {}}
+        for k, v in _values.items():
             result[k] = v == bdb[k]
-            kwargs[k] = bdb[k]
+            if not result[k]:
+                kwargs['not satisfied'][k] = bdb[k]
 
         return all(result.values()), kwargs
