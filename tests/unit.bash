5c67f28c29b1e9782f73e4e846921c04c27677c9
Martin Forstner <martin@redislabs.com>
2020-04-01 18:06:18 +0200

    format test script

--- tests/unit.bash
+++ tests/unit.bash
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 # run 1 RE container
-docker run -d --cap-add sys_resource --name redislabs -p 8443:8443 -p 9443:9443 -p 12000:12000 redislabs/redis
+docker run --detach \
+           --cap-add sys_resource \
+           --name redislabs \
+           --publish 8443:8443 \
+           --publish 9443:9443 \
+           --publish 12000:12000 \
+           redislabs/redis
 sleep 30
 
 # bootstrap the cluster
-docker exec -d --privileged redislabs "/opt/redislabs/bin/rladmin" cluster create name cluster.local username test@redislabs.com password test
+docker exec --detach \
+            --privileged \
+            redislabs "/opt/redislabs/bin/rladmin" cluster create name cluster.local username test@redislabs.com password test
 sleep 10
 
 # create 1 database
-curl -k -u "test@redislabs.com:test" --request POST --url "https://localhost:9443/v1/bdbs" --header 'content-type: application/json' --data '{"name":"db1","type":"redis","memory_size":102400,"port":12000}'
+curl --insecure \
+     --user "test@redislabs.com:test" \
+     --request POST \
+     --url "https://localhost:9443/v1/bdbs" \
+     --header 'content-type: application/json' \
+     --data '{"name":"db1","type":"redis","memory_size":102400,"port":12000}'
 
 # run SUT
 cd ..
-./hc -cfg tests/unit.ini
+./hc --config tests/unit.ini
 ret=${?}
 
 # cleanup
