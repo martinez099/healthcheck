2bd30790254b3d82fdfdf57a0813d84f13ba6442
Martin Forstner <martin@redislabs.com>
2020-04-06 13:47:05 +0200

    increase waiting time between steps

--- tests/unit.bash
+++ tests/unit.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # run 1 RE container
 docker run --detach \
@@ -8,29 +8,36 @@ docker run --detach \
            --publish 9443:9443 \
            --publish 12000:12000 \
            redislabs/redis
-sleep 30
+
+echo "waiting 60 seconds for Docker container to spin up ..."
+sleep 60
 
 # bootstrap the cluster
 docker exec --detach \
             --privileged \
-            redislabs "/opt/redislabs/bin/rladmin" cluster create name cluster.local username test@redislabs.com password test
+            redislabs \
+            "/opt/redislabs/bin/rladmin" cluster create name cluster.local username test@redislabs.com password test
+
+echo "waiting 10 seconds for bootstrap process to finish ..."
 sleep 10
 
-# create 1 database
-curl --insecure \
+# create a database
+curl --silent \
+     --insecure \
      --user "test@redislabs.com:test" \
      --request POST \
      --url "https://localhost:9443/v1/bdbs" \
      --header 'content-type: application/json' \
-     --data '{"name":"db1","type":"redis","memory_size":102400,"port":12000}'
+     --data '{"name":"db1","type":"redis","memory_size":102400,"port":12000}' &> /dev/null
 
 # run SUT
 cd ..
-./hc --config tests/unit.ini
-ret=${?}
+echo "running checks ..."
+./hc --config tests/unit.ini &> /dev/null
+rc=${?}
 
 # cleanup
 docker stop redislabs
 docker rm redislabs
 
-exit ${ret}
+exit ${rc}
